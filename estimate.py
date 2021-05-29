a469f37080493d59c7884d887388dda1fe017b96
LaxmanSC <laxman102102@gmail.com>
2021-05-29 21:55:58 +0530

    Answer for first part

--- estimate.py
+++ estimate.py
@@ -30,3 +30,12 @@ class TestMC(unittest.TestCase):
     
 if __name__ == "__main__":
     unittest.main()
+
+def wallis(n):
+    pi = 1
+    for i in range(1,n):
+        pi = pi*(4*(i**2))/(4*(i**2) - 1)
+    pi = pi * 2
+    return pi
+    
+    
